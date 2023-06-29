68761ea43660c154f39ef6c9f30c3fceb782a301
uppnrise <mtbnacar@gmail.com>
2023-06-30 00:38:13 +0200

    - created an Article model class to have simpler code. MVC: Fat Models, Skinny Controllers.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import {Component, HostBinding, OnInit} from '@angular/core';
+import {Article} from "./article.model";
 
 @Component({
   selector: 'app-article',
@@ -10,28 +11,27 @@ export class ArticleComponent implements OnInit {
   @HostBinding('attr.class')
   cssClass = 'row';
 
-  votes: number;
-  title: string;
-  link: string;
+  article: Article;
 
   constructor() {
-    this.title = 'Angular';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article(
+      'Angular',
+      'http://angular.io',
+      10
+    )
   }
 
   voteUp(): boolean {
-    this.votes += 1;
+    this.article.voteUp();
     return false;
   }
 
   voteDown(): boolean {
-    this.votes -= 1;
+    this.article.voteDown();
     return false;
   }
 
   ngOnInit(): void {
-
   }
 
 }
---
+++ src/app/article/article.model.ts
@@ -0,0 +1,34 @@
+export class Article {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes ?? 0;
+  }
+
+  voteUp(): boolean {
+    this.votes += 1;
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.votes -= 1;
+    return false;
+  }
+
+  /**
+   * A utility function that extracts the domain from a URL.
+   */
+  domain(): string | null {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (err) {
+      return null;
+    }
+  }
+
+}
